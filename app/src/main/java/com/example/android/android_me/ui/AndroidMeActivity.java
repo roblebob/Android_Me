7a4092e7929a4dda5bd159b1124096c872a38d1a
roblebob <robert.koppisch@gmail.com>
2020-10-22 23:06:37 +0200

    ...

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -16,7 +16,7 @@
 
 package com.example.android.android_me.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.android.android_me.R;
@@ -24,7 +24,7 @@ import com.example.android.android_me.R;
 // This activity will display a custom Android image composed of three body parts: head, body, and legs
 public class AndroidMeActivity extends AppCompatActivity {
 
-    // TODO (1) Create a layout file that displays one body part image named fragment_body_part.xml
+    // TODO☑ (1) Create a layout file that displays one body part image named fragment_body_part.xml
         // This layout should contain a single ImageView
 
     // TODO (2) Create a new class called BodyPartFragment to display an image of an Android-Me body part
