0b0bae92a346e6f1d8a01870e4790556e614c180
roblebob <robert.koppisch@gmail.com>
2020-11-02 19:32:18 +0100

    ...

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.android.android_me.R;
+
 // After creating the fragment..
 // TODO (4) Create a new Activity named MainActivity and a corresponding layout file that displays a MasterListFragment
 //  Remember, to display a static fragment in a layout file, use the <fragment> tag
@@ -15,9 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
+        setContentView(R.layout.activity_main);
     }
 
 
