f43379c388bc5651b52c1cf9d1e093c32273b219
roblebob <robert.koppisch@gmail.com>
2021-10-06 08:09:13 +0200

    renaming +

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.android.android_me.R;
 
 // After creating the fragment..
-// TODO (4) Create a new Activity named MainActivity and a corresponding layout file that displays a MasterListFragment
+// TODO✓(4) Create a new Activity named MainActivity and a corresponding layout file that displays a MasterListFragment
 //  Remember, to display a static fragment in a layout file, use the <fragment> tag
 
 public class MainActivity extends AppCompatActivity {
--- app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -1,41 +1,39 @@
 package com.example.android.android_me.ui;
 
 import android.os.Bundle;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import com.example.android.android_me.R;
 import com.example.android.android_me.data.AndroidImageAssets;
 
-// TODO☑ (2) Create a new class called MasterListFragment which will display the GridView list of ALL AndroidMe images
+// TODO✓(2) Create a new class called MasterListFragment which will display the GridView list of ALL AndroidMe images
 //  In the fragment class, you'll need to implement an empty constructor, and onCreateView
 
 public class MasterListFragment extends Fragment {
 
-    public MasterListFragment() { }
+    public MasterListFragment() {
+    }
 
-
-    // TODO☑ (3) In the MasterListFragment class, create a new MasterListAdapter and set it on the GridView
+    // TODO✓(3) In the MasterListFragment class, create a new MasterListAdapter and set it on the GridView
     //  The MasterListAdapter code is provided; it creates the ImageViews that are contained in the GridView
     //  The adapter takes as parameters (Context context, List<Integer> imageIds)
-    MasterListAdapter mMasterListAdapter;
 
 
-    @Nullable
     @Override
-    public View onCreateView(  @NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(  LayoutInflater inflater, ViewGroup container,
+                               Bundle savedInstanceState) {
 
-        mMasterListAdapter =  new MasterListAdapter(  this.getContext(), AndroidImageAssets.getAll() );
+        final View rootView = inflater .inflate(  R.layout.fragment_master_list,  container,  false);
 
-        View rootView = inflater .inflate(  R.layout.fragment_master_list,  container,  false);
-        GridView gridView = (GridView) rootView.findViewById(R.id.images_grid_view);
-        gridView.setAdapter(mMasterListAdapter);
+        GridView gridView = rootView.findViewById(R.id.images_grid_view);
+
+        MasterListAdapter mAdapter =  new MasterListAdapter( getContext(), AndroidImageAssets.getAll() );
+
+        gridView.setAdapter(mAdapter);
 
         return rootView;
     }
